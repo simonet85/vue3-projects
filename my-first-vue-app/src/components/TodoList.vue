6ae779107bd9a32f3e6a6200dffa1a94e7b52219
simonet85 <christian.85@live.fr>
2024-07-28 16:00:44 +0000

    Create ListItem and Todos

---
+++ my-first-vue-app/src/components/TodoList.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    import ListItem from './ListItem.vue';
+</script>
+<template>
+    <ul>
+        <ListItem :is-checked="false">This is the slotted content</ListItem>
+    </ul>
+</template>
+<style scoped>
+    ul {
+        list-style: none;
+    }   
+</style>
