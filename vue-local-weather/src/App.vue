f5376174213fb9484ce658d3232180ee5968e2b8
simonet85 <christian.85@live.fr>
2024-07-28 21:29:09 +0000

    Initialize the App

---
+++ vue-local-weather/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import GetLocation from './components/GetLocation.vue';
+</script>
+
+<template>
+
+
+  <main>
+    <GetLocation />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-local-weather/src/components/GetLocation.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import type { Ref } from 'vue';
+
+type Geolocation = {
+    latitude: number;
+    longitude: number;
+};
+
+const coords: Ref<Geolocation | undefined> = ref();
+const geolocationBlockedByUser: Ref<Boolean> = ref(false);
+
+const getGeolocation = async () : Promise<void> => {
+     await navigator.geolocation.getCurrentPosition(
+        async(position: {coords: Geolocation}) => { coords.value = position.coords
+            // console.log(position.coords);
+        },
+        (error: {message: string}) => {
+            geolocationBlockedByUser.value = true;
+            console.error(error.message);
+        }
+    )
+};
+
+onMounted(async () => await getGeolocation());
+</script>
+<template>
+    <div v-if="coords && !geolocationBlockedByUser">{{ coords.latitude }} {{ coords.longitude }}</div>
+    <div v-if="geolocationBlockedByUser">User denied access</div>
+</template>
