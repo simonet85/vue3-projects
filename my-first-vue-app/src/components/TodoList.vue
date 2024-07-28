a43f3fa760f5af6d4807803a77310da5e1b633f9
simonet85 <christian.85@live.fr>
2024-07-28 19:24:45 +0000

    Add Localstorage functionality

--- my-first-vue-app/src/components/TodoList.vue
+++ my-first-vue-app/src/components/TodoList.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-    import { ref, computed } from 'vue';
+    import { ref, computed, onMounted } from 'vue';
     import type { Ref } from 'vue';
     import ListItem from './ListItem.vue';
     type Item = {
         title: string,
         checked?: boolean
     };
+
     const listItems: Ref<Item[]> = ref([
         {
             title: 'Make a todo list app',
@@ -44,18 +45,77 @@
         const updatedItem = findItemInList(item)
         if (updatedItem) {
             toggleItemChecked(updatedItem)
+            setToStorage(storageItems.value)
         }
     }
     const findItemInList = (item: Item): Item | undefined => {
-        return listItems.value.find((itemInList: Item) => itemInList.title === item.title)
+        return storageItems.value.find((itemInList: Item) => itemInList.title === item.title)
     }
     const toggleItemChecked = (item: Item): void  => {
         item.checked = !item.checked
     }
 
     const sortedList = computed(() => {
-        return [...listItems.value].sort((a, b) => (a.checked ? 1 : 0) - (b.checked ? 1 : 0));
+        return [...storageItems.value].sort((a, b) => (a.checked ? 1 : 0) - (b.checked ? 1 : 0));
     });
+    const setToStorage = (items: Item[]) => {
+        localStorage.setItem('list-items', JSON.stringify(items))
+    }
+
+    const getFromStorage = (): Item[] => {
+        const stored = localStorage.getItem('list-items')
+        if(stored) {
+            return JSON.parse(stored)
+        }
+        return []
+    }
+
+    const storageItems: Ref<Item[]> = ref(getFromStorage())
+
+    const initListItems = ():void => {
+            if(storageItems.value?.length===0) {
+                const listItems = [
+            {
+                title: 'Make a todo list app',
+                checked: true
+            },
+            {
+                title: 'Predict the weather',
+                checked: false
+            },
+            {
+                title: 'Play some tunes',
+                checked: false
+            },
+            {
+                title: 'Get some sleep',
+                checked: false
+            },
+            {
+                title: 'Pump some iron',
+                checked: false
+            },
+            {
+                title: 'Have some fun',
+                checked: false
+            },
+            {
+                title: 'Track my expenses',
+                checked: false
+            },
+            {
+                title: 'Have some fun wit kids',
+                checked: false
+            }
+        ]
+        setToStorage(listItems)
+        storageItems.value = listItems
+        }
+    }
+    onMounted(() => {
+        initListItems()
+        storageItems.value = getFromStorage()
+    })
 </script>
 <template>
     <ul>
